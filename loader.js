c2727d570fe3f80ab1eadd419c3ffc1d7e46283b
Clay Murray <clay.murray8@gmail.com>
2013-08-09 14:12:19 -0600

    Initial Commit
    Properly fetches files with example of using handlebars as a middle man

---
+++ loader.js
@@ -0,0 +1,37 @@
+window.loader = (function(){
+	var l;
+	var cb;
+	window.onload = function(){
+		loader();
+	}
+	function loader(){
+		var els = document.querySelectorAll("[data-load]");
+		[].forEach.call(els, function(el){
+			var src = el.getAttribute("data-load");
+			var xhrReq = new XMLHttpRequest();
+			xhrReq.onload = function(){
+                var html = gotFile.call(this);
+                var dom = HTMLParser(html);
+                el.innerHTML = dom;
+            }
+            xhrReq.open("get", src);
+            xhrReq.send();
+		});
+	}
+    function gotFile(){
+        var response = this.responseText;
+        console.log(this.responseText);
+        var text = cb && cb(response);
+        return text || response;
+    }
+    function HTMLParser(html){
+        html = html.replace(/<script.*?>[\s\S]*?<\/script>/ig, "");
+        return html;
+    }
+	l = {
+		onLoad: function(c){
+			cb = c;
+		}
+	}
+    return l;
+}())
