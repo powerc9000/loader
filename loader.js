5a75976986d5a8e04f5232ab1518ec3408cc7d92
Clay Murray <clay.murray8@gmail.com>
2013-08-09 15:12:08 -0600

    Getting an error will not run the onload function.

--- loader.js
+++ loader.js
@@ -1,26 +1,28 @@
 window.loader = (function(){
-	var l;
-	var cb;
-	window.onload = function(){
-		loader();
-	}
-	function loader(){
-		var els = document.querySelectorAll("[data-load]");
-		[].forEach.call(els, function(el){
-			var src = el.getAttribute("data-load");
-			var xhrReq = new XMLHttpRequest();
-			xhrReq.onload = function(){
-                var html = gotFile.call(this);
-                var dom = HTMLParser(html);
-                el.innerHTML = dom;
+    var l;
+    var cb;
+    window.onload = function(){
+       loader();
+    }
+    function loader(){
+       var els = document.querySelectorAll("[data-load]");
+       [].forEach.call(els, function(el){
+         var src = el.getAttribute("data-load");
+         var xhrReq = new XMLHttpRequest();
+         xhrReq.onload = function(){
+                if(this.status === 200){
+                    var html = gotFile.call(this);
+                    var dom = HTMLParser(html);
+                    el.innerHTML = dom;
+                }
+               
             }
             xhrReq.open("get", src);
             xhrReq.send();
-		});
-	}
+        });
+    }
     function gotFile(){
         var response = this.responseText;
-        console.log(this.responseText);
         var text = cb && cb(response);
         return text || response;
     }
@@ -28,10 +30,10 @@ window.loader = (function(){
         html = html.replace(/<script.*?>[\s\S]*?<\/script>/ig, "");
         return html;
     }
-	l = {
-		onLoad: function(c){
-			cb = c;
-		}
-	}
+    l = {
+       onLoad: function(c){
+         cb = c;
+       }
+    }
     return l;
 }())
